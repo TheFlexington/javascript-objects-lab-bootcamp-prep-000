cd0f9a7f96f8dfc4e8b134eebca46138b10f91a9
Felix Martinez <felixaviermartinez@gmail.com>
2019-07-07 15:44:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   obj.key = 2;
-  return recipes;
+  return obj;
 }
 
 function deleteFromObjectByKey(obj, key) {
