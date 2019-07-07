5ee80b0a39c19f52ed4499fd6100b8d5c35e9b94
Felix Martinez <felixaviermartinez@gmail.com>
2019-07-07 16:09:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(obj, key, value) {
-  var newObj = Object.assign(obj, {});
+  var newObj = Object.assign(obj, {key});
   return newObj;
 }
 
